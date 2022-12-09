3a16eaa8d12b7f0fbf79dabdca2078c8f842faf8
Jervx <louellagracechua@gmail.com>
2022-12-09 15:03:15 +0800

    remove middleware

--- prd_client/pages/_app.js
+++ prd_client/pages/_app.js
@@ -6,5 +6,5 @@ function MyApp({ Component, pageProps }) {
   const getLayout = Component.getLayout || ((page) => page);
   return getLayout(<Component {...pageProps} />);
 }
-
+ 
 export default MyApp;
