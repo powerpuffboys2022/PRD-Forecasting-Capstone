6d95bc227a5d06acb9db96cf03919f62c20c3a81
Jervx <louellagracechua@gmail.com>
2022-12-10 17:06:44 +0800

    Fixed EsLint capital component name

--- prd_client/pages/index.js
+++ prd_client/pages/index.js
@@ -1,7 +1,7 @@
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 
-const index = () => {
+const Index = () => {
   const router = useRouter();
 
   const loadUser = () => {
@@ -54,4 +54,4 @@ const index = () => {
   );
 };
 
-export default index;
+export default Index;
