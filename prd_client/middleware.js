43cf61c2b4829db85e136fe62cbf75cadd619867
Jervx <louellagracechua@gmail.com>
2022-12-10 18:12:26 +0800

    Hotfix : too much redirect -> fixed middleware

--- prd_client/middleware.js
+++ prd_client/middleware.js
@@ -8,7 +8,7 @@ const middleware = async (req) => {
   const jwt = cookies.get("authorization_token");
   const url = req.url;
 
-  if (url.includes("/shop") || url.includes("/admin")) {
+  if(url.includes("/shop")){
     if (!jwt) return NextResponse.redirect(new URL("/login", req.url));
     try {
       const { payload, protectedHeader } = await jose.jwtVerify(
@@ -19,11 +19,45 @@ const middleware = async (req) => {
           audience: "prd",
         }
       );
-      return payload.userType === 1 ? NextResponse.redirect(new URL("/admin", req.url)) : NextResponse.redirect(new URL("/shop", req.url));
+      return payload.userType === 0 ? NextResponse.next() : NextResponse.redirect(new URL("/notallowed", req.url));
     } catch (e) {
       return NextResponse.redirect(new URL("/login", req.url));
     }
   }
+
+  if(url.includes("/admin")){
+    if (!jwt) return NextResponse.redirect(new URL("/login", req.url));
+    try {
+      const { payload, protectedHeader } = await jose.jwtVerify(
+        jwt.value,
+        new TextEncoder().encode(process.env.SECRET),
+        {
+          issuer: "prd",
+          audience: "prd",
+        }
+      );
+      return payload.userType === 1 ? NextResponse.next() : NextResponse.redirect(new URL("/notallowed", req.url));
+    } catch (e) {
+      return NextResponse.redirect(new URL("/login", req.url));
+    }
+  }
+
+//   if (url.includes("/shop") || url.includes("/admin")) {
+//     if (!jwt) return NextResponse.redirect(new URL("/login", req.url));
+//     try {
+//       const { payload, protectedHeader } = await jose.jwtVerify(
+//         jwt.value,
+//         new TextEncoder().encode(process.env.SECRET),
+//         {
+//           issuer: "prd",
+//           audience: "prd",
+//         }
+//       );
+//       return payload.userType === 1 ? NextResponse.redirect(new URL("/admin", req.url)) : NextResponse.redirect(new URL("/shop", req.url));
+//     } catch (e) {
+//       return NextResponse.redirect(new URL("/login", req.url));
+//     }
+//   }
 
   return NextResponse.next();
 };
---
+++ prd_client/pages/notallowed.js
@@ -0,0 +1,34 @@
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import Head from "next/head";
+
+
+const NotAllowed = () => {
+  const router = useRouter();
+
+  return (
+    <div className="bg-3">
+      <Head>
+        <title>Philip Rice Dealer</title>
+        <meta
+          name="description"
+          content="Philip Rice Dealer Online store & forecasting"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <section
+        className={
+          "backdrop-blur-lg font-inter h-screen w-full flex justify-center items-center " +
+          ``
+        }
+      >
+        <div className="mx-8 md:mx-0 bg-base-100/40 p-4 md:p-8 rounded-lg">
+            <p className="md:text-lg text-center">Your account is not allowed on this page</p>
+            <button onClick={()=>{router.back()}} className="w-full mt-6 btn btn-ghost btn-outline">Go Back</button>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default NotAllowed;
